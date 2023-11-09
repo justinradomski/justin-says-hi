40457ece160524ed9115e96afe4ddb1a8632b12c
Justin Radomski <justin@doubleupdigital.com>
2023-11-09 09:29:38 -0500

    website

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <span className="w-full text-center">hi.</span>          
+      </div>
+    </main>
+  )
+}
